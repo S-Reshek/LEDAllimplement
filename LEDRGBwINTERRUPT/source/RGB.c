9bca2511fb963b72bc0a2f862702a9d2ef5ec8b0
Resh <ie698356@iteso.mx>
2018-09-17 15:21:21 -0500

    listo para agregar interrupts, no errors

--- LEDRGBwINTERRUPT/source/RGB.c
+++ LEDRGBwINTERRUPT/source/RGB.c
@@ -12,31 +12,31 @@
 
 void RGB_init(void)
 {
-	gpio_pin_control_register_t led_config = GPIO_MUX1;
-	gpio_pin_control_register_t sw_config = GPIO_MUX1 | GPIO_PE | GPIO_PS;
+	GPIO_pinControlRegisterType led_config = GPIO_MUX1;
+	GPIO_pinControlRegisterType sw_config = GPIO_MUX1 | GPIO_PE | GPIO_PS;
 
-	GPIO_clock_gating(BLUE_LED_PORT);
-	GPIO_clock_gating(RED_LED_PORT);
-	GPIO_clock_gating(GREEN_LED_PORT);
+	GPIO_clockGating(BLUE_LED_PORT);
+	GPIO_clockGating(RED_LED_PORT);
+	GPIO_clockGating(GREEN_LED_PORT);
 
-	GPIO_clock_gating(SW2_PORT);
-	GPIO_clock_gating(SW3_PORT);
+	GPIO_clockGating(SW2_PORT);
+	GPIO_clockGating(SW3_PORT);
 
 
-	GPIO_set_pin(RED_LED_PORT, RED_LED_PIN);
-	GPIO_set_pin(BLUE_LED_PORT, BLUE_LED_PIN);
-	GPIO_set_pin(GREEN_LED_PORT, GREEN_LED_PIN);
+	GPIO_setPIN(RED_LED_PORT, RED_LED_PIN);
+	GPIO_setPIN(BLUE_LED_PORT, BLUE_LED_PIN);
+	GPIO_setPIN(GREEN_LED_PORT, GREEN_LED_PIN);
 
-	GPIO_pin_control_register(BLUE_LED_PORT, BLUE_LED_PIN, &led_config);
-	GPIO_pin_control_register(RED_LED_PORT, RED_LED_PIN, &led_config);
-	GPIO_pin_control_register(GREEN_LED_PORT, GREEN_LED_PIN, &led_config);
+	GPIO_pinControlRegister(BLUE_LED_PORT, BLUE_LED_PIN, &led_config);
+	GPIO_pinControlRegister(RED_LED_PORT, RED_LED_PIN, &led_config);
+	GPIO_pinControlRegister(GREEN_LED_PORT, GREEN_LED_PIN, &led_config);
 
-	GPIO_pin_control_register(SW2_PORT, SW2_PIN, &sw_config);
-	GPIO_pin_control_register(SW3_PORT, SW3_PIN, &sw_config);
+	GPIO_pinControlRegister(SW2_PORT, SW2_PIN, &sw_config);
+	GPIO_pinControlRegister(SW3_PORT, SW3_PIN, &sw_config);
 
-	GPIO_data_direction_pin(BLUE_LED_PORT, GPIO_OUTPUT, BLUE_LED_PIN);
-	GPIO_data_direction_pin(RED_LED_PORT, GPIO_OUTPUT, RED_LED_PIN);
-	GPIO_data_direction_pin(GREEN_LED_PORT, GPIO_OUTPUT, GREEN_LED_PIN);
+	GPIO_dataDirectionPIN(BLUE_LED_PORT, GPIO_OUTPUT, BLUE_LED_PIN);
+	GPIO_dataDirectionPIN(RED_LED_PORT, GPIO_OUTPUT, RED_LED_PIN);
+	GPIO_dataDirectionPIN(GREEN_LED_PORT, GPIO_OUTPUT, GREEN_LED_PIN);
 
 
 
@@ -51,8 +51,8 @@ Colors_t RGB_color_selector(void)
 	uint8_t total_color = 0;
 	static Colors_t color = RED;
 
-	input_a = GPIO_read_pin(SW2_PORT, SW2_PIN);
-	input_c = GPIO_read_pin(SW3_PORT, SW3_PIN);
+	input_a = GPIO_readPIN(SW2_PORT, SW2_PIN);
+	input_c = GPIO_readPIN(SW3_PORT, SW3_PIN);
 
 	total_color = (input_a << 1) | input_c;
 
@@ -82,8 +82,8 @@ Colors_t RGB_color_selector(void)
 
 	do
 	{
-		input_a = GPIO_read_pin(SW2_PORT, SW2_PIN);
-		input_c = GPIO_read_pin(SW3_PORT, SW3_PIN);
+		input_a = GPIO_readPIN(SW2_PORT, SW2_PIN);
+		input_c = GPIO_readPIN(SW3_PORT, SW3_PIN);
 		total_color = (input_a << 1) | input_c;
 		total_color &= SW_MASK;
 
@@ -99,11 +99,11 @@ void RGB_red_led_on_off(uint8_t on_off)
 {
 	if (LED_ON == on_off )
 	{
-		GPIO_clear_pin(RED_LED_PORT, RED_LED_PIN);
+		GPIO_clearPIN(RED_LED_PORT, RED_LED_PIN);
 	}
 	else
 	{
-		GPIO_set_pin(RED_LED_PORT, RED_LED_PIN);
+		GPIO_setPIN(RED_LED_PORT, RED_LED_PIN);
 	}
 
 }
@@ -112,11 +112,11 @@ void RGB_blue_led_on_off(uint8_t on_off)
 {
 	if (LED_ON == on_off )
 	{
-		GPIO_clear_pin(BLUE_LED_PORT, BLUE_LED_PIN);
+		GPIO_clearPIN(BLUE_LED_PORT, BLUE_LED_PIN);
 	}
 	else
 	{
-		GPIO_set_pin(BLUE_LED_PORT, BLUE_LED_PIN);
+		GPIO_setPIN(BLUE_LED_PORT, BLUE_LED_PIN);
 	}
 }
 
@@ -124,11 +124,11 @@ void RGB_green_led_on_off(uint8_t on_off)
 {
 	if (LED_ON == on_off )
 	{
-		GPIO_clear_pin(GREEN_LED_PORT, GREEN_LED_PIN);
+		GPIO_clearPIN(GREEN_LED_PORT, GREEN_LED_PIN);
 	}
 	else
 	{
-		GPIO_set_pin(GREEN_LED_PORT, GREEN_LED_PIN);
+		GPIO_setPIN(GREEN_LED_PORT, GREEN_LED_PIN);
 	}
 }
 
@@ -136,41 +136,41 @@ void RGB_yellow_led_on_off(uint8_t on_off)
 {
 	if (LED_ON == on_off )
 	{
-		GPIO_clear_pin(GREEN_LED_PORT, GREEN_LED_PIN);
-		GPIO_clear_pin(RED_LED_PORT, RED_LED_PIN);
+		GPIO_clearPIN(GREEN_LED_PORT, GREEN_LED_PIN);
+		GPIO_clearPIN(RED_LED_PORT, RED_LED_PIN);
 	}
 	else
 	{
-		GPIO_set_pin(GREEN_LED_PORT, GREEN_LED_PIN);
-		GPIO_set_pin(RED_LED_PORT, RED_LED_PIN);
+		GPIO_setPIN(GREEN_LED_PORT, GREEN_LED_PIN);
+		GPIO_setPIN(RED_LED_PORT, RED_LED_PIN);
 	}
 }
 void RGB_white_led_on_off(uint8_t on_off)
 {
 	if (LED_ON == on_off )
 	{
-		GPIO_clear_pin(GREEN_LED_PORT, GREEN_LED_PIN);
-		GPIO_clear_pin(RED_LED_PORT, RED_LED_PIN);
-		GPIO_clear_pin(BLUE_LED_PORT, BLUE_LED_PIN);
+		GPIO_clearPIN(GREEN_LED_PORT, GREEN_LED_PIN);
+		GPIO_clearPIN(RED_LED_PORT, RED_LED_PIN);
+		GPIO_clearPIN(BLUE_LED_PORT, BLUE_LED_PIN);
 	}
 	else
 	{
-		GPIO_set_pin(GREEN_LED_PORT, GREEN_LED_PIN);
-		GPIO_set_pin(RED_LED_PORT, RED_LED_PIN);
-		GPIO_set_pin(BLUE_LED_PORT, BLUE_LED_PIN);
+		GPIO_setPIN(GREEN_LED_PORT, GREEN_LED_PIN);
+		GPIO_setPIN(RED_LED_PORT, RED_LED_PIN);
+		GPIO_setPIN(BLUE_LED_PORT, BLUE_LED_PIN);
 	}
 }
 void RGB_purple_led_on_off(uint8_t on_off)
 {
 	if (LED_ON == on_off )
 	{
-		GPIO_clear_pin(BLUE_LED_PORT, BLUE_LED_PIN);
-		GPIO_clear_pin(RED_LED_PORT, RED_LED_PIN);
+		GPIO_clearPIN(BLUE_LED_PORT, BLUE_LED_PIN);
+		GPIO_clearPIN(RED_LED_PORT, RED_LED_PIN);
 	}
 	else
 	{
-		GPIO_set_pin(BLUE_LED_PORT, BLUE_LED_PIN);
-		GPIO_set_pin(RED_LED_PORT, RED_LED_PIN);
+		GPIO_setPIN(BLUE_LED_PORT, BLUE_LED_PIN);
+		GPIO_setPIN(RED_LED_PORT, RED_LED_PIN);
 	}
 }
 
--- LEDRGBwINTERRUPT/source/main.c
+++ LEDRGBwINTERRUPT/source/main.c
@@ -1,12 +1,7 @@
 
 #include <stdio.h>
-#include "board.h"
-#include "peripherals.h"
-#include "pin_mux.h"
-#include "clock_config.h"
 #include "MK64F12.h"
-#include "fsl_debug_console.h"
-
+#include "Bits.h"
 #include "RGB.h"
 #include "Delay.h"
 
