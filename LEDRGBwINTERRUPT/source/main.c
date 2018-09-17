7426c320ac81fc86614e944e7aa9c4800f922b1b
Resh <ie698356@iteso.mx>
2018-09-17 14:47:50 -0500

    iniciando RGB con interrupts, based NVIC/PIT

---
+++ LEDRGBwINTERRUPT/source/main.c
@@ -0,0 +1,73 @@
+
+#include <stdio.h>
+#include "board.h"
+#include "peripherals.h"
+#include "pin_mux.h"
+#include "clock_config.h"
+#include "MK64F12.h"
+#include "fsl_debug_console.h"
+
+#include "RGB.h"
+#include "Delay.h"
+
+
+
+int main(void) {
+
+
+	Colors_t color = NO_COLOR;
+
+	RGB_init();
+
+	RGB_red_led_on_off(LED_ON);
+	delay(DELAY);
+	RGB_red_led_on_off(LED_OFF);
+	delay(DELAY);
+	RGB_blue_led_on_off(LED_ON);
+	delay(DELAY);
+	RGB_blue_led_on_off(LED_OFF);
+	delay(DELAY);
+	RGB_green_led_on_off(LED_ON);
+	delay(DELAY);
+	RGB_green_led_on_off(LED_OFF);
+	delay(DELAY);
+	RGB_yellow_led_on_off(LED_ON);
+	delay(DELAY);
+	RGB_yellow_led_on_off(LED_OFF);
+	delay(DELAY);
+	RGB_white_led_on_off(LED_ON);
+	delay(DELAY);
+	RGB_white_led_on_off(LED_OFF);
+	delay(DELAY);
+	RGB_purple_led_on_off(LED_ON);
+	delay(DELAY);
+	RGB_purple_led_on_off(LED_OFF);
+	delay(DELAY);
+
+    while(1) {
+
+    	color = RGB_color_selector();
+
+    	switch (color) {
+    		case GREEN:
+    			RGB_green();
+    		break;
+    		case BLUE:
+    			RGB_blue();
+    		break;
+    		case PURPLE:
+    			RGB_purple();
+    		break;
+			case RED:
+				RGB_red();
+			break;
+			case YELLOW:
+				RGB_red();
+			default:
+				RGB_yellow();
+			break;
+		}
+
+    }
+    return 0 ;
+}
